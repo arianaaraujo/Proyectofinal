821507c8fbda27a885be8e7d3dfe849bac35c8f9
francesca-rivas <39887926+francesca-rivas@users.noreply.github.com>
2019-10-21 12:01:37 -0500

    Rename main.cpp to main0.cpp

---
+++ main0.cpp
@@ -0,0 +1,27 @@
+//Segunda opción
+
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <string.h>
+#include "funciones.h"
+
+using namespace std;
+
+int main() {
+  string doc,texto2;
+
+  cout<<"---BIENVENIDO AL EDITOR DE TEXTO---"<<endl<<endl;
+  cout<<"Nombre del archivo: ";cin>>doc;cout<<endl;
+
+  ifstream archivo;
+  archivo.open(doc,ios::in);
+
+  if(archivo.fail()){
+    cout<<"El archivo no existe."<<endl;
+    cout<<"Nombre del archivo: ";cin>>doc;cout<<endl;
+  };
+
+  opciones(doc);
+
+}
