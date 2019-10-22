0b31e771ed65067cc936667e51934f25c75d9e7c
Claudia-Pacori <55209248+Claudia-Pacori@users.noreply.github.com>
2019-10-22 08:36:51 -0500

    Update c_funciones.h

--- c_funciones.h
+++ c_funciones.h
@@ -1,9 +1,9 @@
 using namespace std;
 string leer(string texto);
 void mostrar(string texto);
-void derecha(string texto);
+void derecha(string &texto);
 void centrar(string &texto);
-void opciones(string texto,string cache);
+void opciones(string texto,string cache,string cache2);
 void izquierda(string texto);
 void text();
 void print(string cache);
