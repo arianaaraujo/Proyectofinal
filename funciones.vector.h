169d5bae88f18c9eb4a0e7f03facdb52ea9ae0c6
arianaaraujo <40767338+arianaaraujo@users.noreply.github.com>
2019-10-24 11:25:01 -0500

    Add files via upload

---
+++ funciones.vector.h
@@ -0,0 +1,6 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+void leer(vector<vector<string>>&texto);
+void opciones(vector<vector<string>>&texto);
+
---
+++ main.vector.cpp
@@ -0,0 +1,10 @@
+#include "funciones.vector.h"
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int main () {
+    vector<vector<string>>texto;
+  opciones(texto);
+  return 0;
+}
