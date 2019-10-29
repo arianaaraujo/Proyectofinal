62914c908b4490e7ebfa51b8178acfd05cb9c1f9
Claudia-Pacori <55209248+Claudia-Pacori@users.noreply.github.com>
2019-10-28 21:06:38 -0500

    Update c_funciones.h

--- c_funciones.h
+++ c_funciones.h
@@ -1,9 +1,11 @@
+#include <vector>
 using namespace std;
 string leer(string texto);
+void guardar_string(string cache, string texto);
 void mostrar(string texto);
 void derecha(string &texto);
 void centrar(string &texto);
-void opciones(string texto,string cache,string cache2);
+void opciones(string texto,string cache,string cache2,vector<vector<string>> cache_vector,vector<vector<string>>cache_vector2, int anterior);
 void izquierda(string texto);
 void text();
 void print(string cache);
