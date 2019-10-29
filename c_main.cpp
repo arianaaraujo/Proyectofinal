92414d9d5b949a5efb2f62e3105a0e11ca32f2b3
Claudia-Pacori <55209248+Claudia-Pacori@users.noreply.github.com>
2019-10-28 21:04:47 -0500

    Update c_main.cpp
    Cambios con todas las opciones

--- c_main.cpp
+++ c_main.cpp
@@ -2,14 +2,17 @@
 #include <iomanip>
 #include <fstream>
 #include <string>
+#include <vector>
 #include <stdlib.h>
 #include "funciones.h"
+#include "vectores.h"
 
 using namespace std;
 
 int main() {
   string doc,cache,cache2;
   ifstream archivo;
+  vector<vector<string>> cache_vector;vector<vector<string>>cache_vector2;
 
   cout<<"---BIENVENIDO AL EDITOR DE TEXTO---"<<endl<<endl;
   do{
@@ -19,6 +22,7 @@ int main() {
   }while(archivo.fail());
   cache=leer(doc);
   cache2=cache;
-  opciones(doc,cache,cache2);
-
-}
+  cache_vector = leer_vector(doc);
+  cache_vector2 = cache_vector;
+  int anterior=100;
+  opciones(doc,cache,cache2,cache_vector,cache_vector2,anterior);}
